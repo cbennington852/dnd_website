6292f1c85d6dfedd8bf4f2d3c8338cd3b0be8f9b
Charles Bennington <c.bennington852@gmail.com>
2026-04-18 20:51:29 -0700

    feat : new misisons

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Delta Green : Code Red",
+    pageTitle: "",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
